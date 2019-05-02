f5c557fce26c8800c2730bbfe3bd6721bb09f8f4
Khrystyna <l.xrustja@gmail.com>
2019-05-02 15:46:46 +0300

    added content on page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    $('.menu__btn').click(function() {
+        $('.menu').slideToggle();
+        $(this).toggleClass('open');
+        if($(this).hasClass('open')){
+            $(this).text('закрити меню').append("<span></span>");         
+        } else {
+            $(this).text('меню').append("<span></span>");
+        }
+    });
 
+    $('.search__btn').click(function(){
+        $(this).prev().toggleClass('open')
+    });
 
 });
